605b6f0806a4dcb580a04b409480bb6c225e5de0
RogerSelwyn <RogerSelwyn@users.noreply.github.com>
2022-02-01 11:04:49 +0000

    Update update_requirements.py

--- release-asset/helpers/update_requirements.py
+++ release-asset/helpers/update_requirements.py
@@ -5,16 +5,16 @@ import sys
 
 COMPONENT = sys.argv[2]
 
-harequire = []
-request = requests.get(
-    "https://raw.githubusercontent.com/home-assistant/home-assistant/dev/setup.py"
-)
-request = request.text.split("REQUIRES = [")[1].split("]")[0].split("\n")
-for req in request:
-    if "=" in req:
-        harequire.append(req.split(">")[0].split("=")[0].split('"')[1])
+# harequire = []
+# request = requests.get(
+#     "https://raw.githubusercontent.com/home-assistant/home-assistant/dev/setup.py"
+# )
+# request = request.text.split("REQUIRES = [")[1].split("]")[0].split("\n")
+# for req in request:
+#     if "=" in req:
+#         harequire.append(req.split(">")[0].split("=")[0].split('"')[1])
 
-print(harequire)
+# print(harequire)
 
 with open(f"{os.getcwd()}/custom_components/{COMPONENT}/manifest.json", "r") as manifest:
     manifest = json.load(manifest)
@@ -22,9 +22,9 @@ with open(f"{os.getcwd()}/custom_components/{COMPONENT}/manifest.json", "r") as 
     for req in manifest["requirements"]:
         requirements.append(req)
 
-for req in requirements:
-    if req.split(">")[0].split("=")[0] in harequire:
-        print(f"{req.split('>')[0].split('=')[0]} in HA requirements, no need here.")
+# for req in requirements:
+#     if req.split(">")[0].split("=")[0] in harequire:
+#         print(f"{req.split('>')[0].split('=')[0]} in HA requirements, no need here.")
 print(json.dumps(manifest["requirements"], indent=4, sort_keys=True))
 
 with open(f"{os.getcwd()}/requirements.txt", "w") as requirementsfile:
