7528074caed7107e39b241117fb78390d6a25a95
RogerSelwyn <rogers@selwyn.org.uk>
2023-09-10 11:49:31 +0100

    Improve release note generation

--- release-notes/helpers/generate_releasenotes.py
+++ release-notes/helpers/generate_releasenotes.py
@@ -1,6 +1,7 @@
 """Create the release notes."""
 import re
 import sys
+from datetime import datetime
 
 from github import Github
 
@@ -12,16 +13,12 @@ BODY = """[![Downloads for this release](https://img.shields.io/github/downloads
 
 """
 
-CHANGES = """
-### Breaking Changes
-### Enhancements
-### Fixes
-### Maintenance
-### Documentation
-
-{repo_changes}
-
-"""
+CHANGES_BREAKING = "### 💥 Breaking Changes"
+CHANGES_ENHANCEMENTS = "### ✨ Enhancements"
+CHANGES_FIXES = "### 🐛 Fixes"
+CHANGES_MAINTENANCE = "### ♻️  Maintenance"
+CHANGES_DOCUMENTATION = "### ⚡ Documentation"
+CHANGES_OTHER = "### Other"
 
 CHANGE = "- [{line}]({link}) - @{author}\n"
 NOCHANGE = "_No changes in this release._"
@@ -31,25 +28,20 @@ GITHUB = Github(sys.argv[2])
 
 def _new_commits(repo, sha):
     """Get new commits in repo."""
-    from datetime import datetime
 
     dateformat = "%a, %d %b %Y %H:%M:%S GMT"
     release_commit = repo.get_commit(sha)
     since = datetime.strptime(release_commit.last_modified, dateformat)
     commits = repo.get_commits(since=since)
-    if len(list(commits)) == 1:
-        return False
-    return reversed(list(commits)[:-1])
+    return False if len(list(commits)) == 1 else reversed(list(commits)[:-1])
 
 
 def _last_repo_release(github, skip=True):
     """Return last release."""
     repo = github.get_repo(REPOSITORY)
     tag_sha = None
-    data = {}
-    tags = list(repo.get_tags())
-    reg = "(v|^)?(\\d+\\.)?(\\d+\\.)?(\\*|\\d+)(b\\d+)?$"
-    if tags:
+    if tags := list(repo.get_tags()):
+        reg = "(v|^)?(\\d+\\.)?(\\d+\\.)?(\\*|\\d+)(b\\d+)?$"
         for tag in tags:
             tag_name = tag.name
             if re.match(reg, tag_name):
@@ -58,33 +50,62 @@ def _last_repo_release(github, skip=True):
                     skip = False
                     continue
                 break
-    data["tag_name"] = tag_name
-    data["tag_sha"] = tag_sha
-    return data
+    return {"tag_name": tag_name, "tag_sha": tag_sha}
+
+
+def _process_chunks(change_title, changes):
+    return change_title + "\n" + changes + "\n" if changes else ""
+
+
+def _add_line(startswith, commit, msg):
+    if "Merge branch " in msg:
+        return
+    if "Merge pull request " in msg:
+        return
+    if "\n" in msg:
+        msg = msg.split("\n")[0]
+    try:
+        author = commit.author.login if commit.author else None
+    except Exception:  # pylint: disable=broad-exception-caught
+        author = None
+    if msg.startswith(startswith):
+        msg = msg[len(startswith) :].strip()
+    return CHANGE.format(line=msg, link=commit.html_url, author=author)
 
 
 def _get_repo_commits(github, skip=True):
     changes = ""
+    repo_changes_breaking = ""
+    repo_changes_enhancement = ""
+    repo_changes_fixes = ""
+    repo_changes_maintenance = ""
+    repo_changes_documentation = ""
+    repo_changes_other = ""
     repo = github.get_repo(REPOSITORY)
-    commits = _new_commits(repo, _last_repo_release(github, skip)["tag_sha"])
-
-    if not commits:
-        changes = NOCHANGE
-    else:
+    if commits := _new_commits(repo, _last_repo_release(github, skip)["tag_sha"]):
         for commit in commits:
             msg = repo.get_git_commit(commit.sha).message
-            if "Merge branch " in msg:
-                continue
-            if "Merge pull request " in msg:
-                continue
-            if "\n" in msg:
-                msg = msg.split("\n")[0]
-            try:
-                author = commit.author.login if commit.author else None
-            except Exception:
-                author = None
-            changes += CHANGE.format(line=msg, link=commit.html_url, author=author)
+            if msg.startswith("break:"):
+                repo_changes_breaking += _add_line("break:", commit, msg)
+            elif msg.startswith("feat:"):
+                repo_changes_enhancement += _add_line("feat:", commit, msg)
+            elif msg.startswith("fix:"):
+                repo_changes_fixes += _add_line("fix:", commit, msg)
+            elif msg.startswith("maint:"):
+                repo_changes_maintenance += _add_line("maint:", commit, msg)
+            elif msg.startswith("doc:"):
+                repo_changes_documentation += _add_line("doc:", commit, msg)
+            else:
+                repo_changes_other += _add_line("", commit, msg)
 
+        changes += _process_chunks(CHANGES_BREAKING, repo_changes_breaking)
+        changes += _process_chunks(CHANGES_ENHANCEMENTS, repo_changes_enhancement)
+        changes += _process_chunks(CHANGES_FIXES, repo_changes_fixes)
+        changes += _process_chunks(CHANGES_MAINTENANCE, repo_changes_maintenance)
+        changes += _process_chunks(CHANGES_DOCUMENTATION, repo_changes_documentation)
+        changes += _process_chunks(CHANGES_OTHER, repo_changes_other)
+    else:
+        changes = NOCHANGE
     return changes
 
 
@@ -98,15 +119,13 @@ if UPDATERELEASE == "yes":
         name=VERSION,
         message=BODY.format(
             version=VERSION,
-            changes=CHANGES.format(
-                repo_changes=_get_repo_commits(GITHUB),
-            ),
+            changes=_get_repo_commits(GITHUB),
             repository=REPOSITORY,
         ),
         prerelease=True,
     )
 else:
-    repo_changes = _get_repo_commits(GITHUB, False)
+    repo_changes = _get_repo_commits(GITHUB, False)  # pylint: disable=invalid-name
     if repo_changes != NOCHANGE:
         VERSION = _last_repo_release(GITHUB, False)["tag_name"]
         VERSION = f"{VERSION[:-1]}{int(VERSION[-1])+1}"
@@ -114,9 +133,7 @@ else:
             title=f"Create release {VERSION}?",
             labels=["New release"],
             assignee=sys.argv[10],
-            body=CHANGES.format(
-                repo_changes=repo_changes,
-            ),
+            body=repo_changes,
         )
     else:
         print("Not enough changes for a release.")
