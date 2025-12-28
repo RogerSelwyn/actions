e7a86a8ce6709170590a1727db3bba42dfb11407
RogerSelwyn <rogers@selwyn.org.uk>
2025-12-28 13:03:43 +0000

    Maintain prettier formatting

--- release-asset/helpers/update_version.py
+++ release-asset/helpers/update_version.py
@@ -15,9 +15,25 @@ manifest_file = f"{os.getcwd()}/custom_components/{COMPONENT}/manifest.json"
 # manifest_file = f"{os.getcwd()}/../manifest.json"
 
 
+def inplace_change(filename, old_string, new_string):
+    # Safely read the input filename using 'with'
+    with open(filename) as f:
+        s = f.read()
+        if old_string not in s:
+            print('"{old_string}" not found in {filename}.'.format(**locals()))
+            return
+
+    # Safely write the changed content, if found in the file
+    with open(filename, "w") as f:
+        print(
+            'Changing "{old_string}" to "{new_string}" in {filename}'.format(**locals())
+        )
+        s = s.replace(old_string, new_string)
+        f.write(s)
+
+
 with open(manifest_file, "r") as manifest:
     manifest_data = json.load(manifest)
-    manifest_data["version"] = version
+    old_version = manifest_data["version"]
 
-with open(manifest_file, "w") as manifest:
-    json.dump(manifest_data, manifest, indent=2)
+inplace_change(manifest_file, old_version, version)
